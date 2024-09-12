e4579bb957e1919f8400a08b2476d82a1e482dbe
Zedric-V-Zamuco <Zedricvzamuco@gmail.com>
2024-09-12 15:04:50 +0800

    first commit

---
+++ Activity.java
@@ -0,0 +1,14 @@
+public class Activity {
+    public static void main (String args[]) {
+        
+        Dog mydog = new Dog();
+        mydog.name = "butete";
+        mydog.isMale = true;
+
+        mydog1.friends[0] = "lapuk";
+        mydog1.friends[1] = "pula";
+
+    
+
+    }
+}
